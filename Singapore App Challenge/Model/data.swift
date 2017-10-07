80b8020fae1898d1997787fd709424d3e693b778
Octavianus Gandajaya <oktagandajaya@gmail.com>
2017-10-07 14:58:22 +0700

    neww

---
+++ Singapore App Challenge/Model/data.swift
@@ -0,0 +1,9 @@
+//
+//  data.swift
+//  Singapore App Challenge
+//
+//  Created by Octavianus Gandajaya on 10/7/17.
+//  Copyright © 2017 Octavianus Gandajaya. All rights reserved.
+//
+
+import Foundation
